37976f314195ae2981123f838c72d1a72b52fb44
SOWMIYA.S <141092918+Sowmi2310@users.noreply.github.com>
2023-09-13 20:32:03 +0530

    JavaScript task

---
+++ var task3.js
@@ -0,0 +1,10 @@
+function detail(){
+var a=2003;
+var b=2003;
+var a1=2026;
+var b2=2028;
+var x1=a1-a;
+var x2=b2-b;
+console.log("I will be either"+" "+x1+" "+"or"+" "+x2+" "+"in"+" "+b2)
+}
+detail();
---
+++ var task4.js
@@ -0,0 +1,9 @@
+function task4(){
+    var a=20;
+    var b=80;
+    var c=100;
+    var age=b-a;
+    var amt=age*100;
+    console.log("You will need" +" "+amt+" "+"last you untill the ripe old age of"+" "+b);
+}
+task4()   
