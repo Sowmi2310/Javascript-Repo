9461f9e5234832b2d490a6a3df3badd676956397
SOWMIYA.S <141092918+Sowmi2310@users.noreply.github.com>
2023-09-27 08:43:23 +0530

    JavaScript task

---
+++ object task1.js
@@ -0,0 +1,10 @@
+let std={
+    details :{
+  name:"Mercedes Benz",
+ model:"C-Class",
+  manufacturer:"Daimler AG company",
+  length:"4687mm",
+  cargocapacity:"12.6 cubic feet"
+    }
+  }
+console.log(std.details)
---
+++ object task2.js
@@ -0,0 +1,14 @@
+var obj=   {
+    title: 'Baahubali',
+    director: 'Rajamouli',
+    producers: [ 'Sobhu', 'Raghavendra', 'Prasad' ],
+    'lead actors with their roles': {
+      prabhas: 'Baahubali',
+      anushka: 'Devasena',
+      rana: 'Bhallaladeva',
+      tamannah: 'Avanthika'
+    }
+  }
+
+  console.log(obj)
+   
